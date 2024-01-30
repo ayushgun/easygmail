9b8011c4d7f54e916b5272d15a71b278e3ca0f67
ayushgun <ayushgundawar@gmail.com>
2024-01-30 12:25:00 -0500

    Added docstrings in Google style

--- easygmail/client.py
+++ easygmail/client.py
@@ -7,6 +7,28 @@ from .utility import validate_gmail, validate_password
 
 
 class Client:
+    """
+    A client for sending emails via Gmail.
+
+    This class manages the connection to the Gmail SMTP server and provides
+    a member function for sending email messages.
+
+    Attributes:
+        email_address (str): Email address of the client.
+        password (str): App password on the client's email account.
+        server (smtplib.SMTP): Instance of SMTP for email transmission.
+
+    Args:
+        env_file (str, optional): Path to the .env file containing EMAIL_ADDRESS
+            and PASSWORD. If not provided, email_address and password must be
+            given directly.
+        email_address (str, optional): The Gmail address from which emails will be sent.
+        password (str, optional): The password for the given Gmail account.
+
+    Raises:
+        ValueError: If the necessary credentials are not provided or invalid.
+    """
+
     def __init__(
         self, env_file: str = None, email_address: str = None, password: str = None
     ) -> None:
@@ -22,10 +44,8 @@ class Client:
 
         if not email_address or not password:
             raise ValueError("Email address and password must be provided")
-
         if not validate_gmail(email_address):
             raise ValueError("Email must be a Gmail address (ends with @gmail.com)")
-
         if not validate_password(password):
             raise ValueError("Password must be at least 8 characters long")
 
@@ -33,17 +53,35 @@ class Client:
         self.password = password
         self.__init_server()
 
+    def __del__(self) -> None:
+        self.server.quit()
+
     def __init_server(self) -> None:
+        """
+        Initializes the SMTP server for sending emails.
+
+        This method sets up the SMTP server with Gmail's settings and logs in
+        using the provided credentials. It is called during the initialization
+        of the Client object.
+        """
+
         self.server = smtplib.SMTP("smtp.gmail.com", 587)
         self.server.ehlo_or_helo_if_needed()
         self.server.starttls()
         self.server.ehlo_or_helo_if_needed()
         self.server.login(self.email_address, self.password)
 
-    def __del__(self) -> None:
-        self.server.quit()
-
     def send(self, email: EmailMessage) -> None:
+        """
+        Sends an email message.
+
+        This method sends an EmailMessage using the initialized SMTP server.
+        If the 'From' field is not set in the email, it uses the client's email address.
+
+        Args:
+            email (EmailMessage): The email message to be sent.
+        """
+
         if "From" not in email:
             email["From"] = self.email_address
 
